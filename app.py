bd140a203bd0752113e48bbcb442637fc4c97318
Gabriel Ovidio Vieira <42852842+gabrielov2003@users.noreply.github.com>
2023-08-09 23:03:04 -0300

    Adiciona rota de pagamento

--- app.py
+++ app.py
@@ -27,3 +27,57 @@ def transport():
         transport_type = 'táxi'
         price = distance * 2.5
     return {'transport_type': transport_type, 'price': price}
+
+
+@app.route('/payment', methods=['POST'])
+def payment():
+    request = app.current_request
+    body = request.json_body
+    transport_type = body.get('transport_type')
+    price = body.get('price')
+    payment_method = body.get('payment_method')
+    if not transport_type or not price or not payment_method:
+        return Response(body={'error': 'Missing transport_type, price or payment_method'},
+                        status_code=400)
+    
+    delay = random.randint(1, 5)
+    time.sleep(delay)
+    
+    if payment_method == 'credit_card':
+        card_number = body.get('card_number')
+        card_expiry_date = body.get('card_expiry_date')
+        card_cvv = body.get('card_cvv')
+        if not card_number or not card_expiry_date or not card_cvv:
+            return Response(body={'error': 'Missing credit card information'},
+                            status_code=400)
+        if len(card_number) != 16 or len(card_cvv) != 3:
+            return Response(body={'error': 'Invalid credit card information'},
+                            status_code=400)
+        return {'status': 'success'}
+    
+    elif payment_method == 'debit_card':
+        card_number = body.get('card_number')
+        card_expiry_date = body.get('card_expiry_date')
+        card_cvv = body.get('card_cvv')
+        if not card_number or not card_expiry_date or not card_cvv:
+            return Response(body={'error': 'Missing debit card information'},
+                            status_code=400)
+        if len(card_number) != 16 or len(card_cvv) != 3:
+            return Response(body={'error': 'Invalid debit card information'},
+                            status_code=400)
+        return {'status': 'success'}
+    
+    elif payment_method == 'cash':
+        amount_received = body.get('amount_received')
+        if not amount_received:
+            return Response(body={'error': 'Missing amount_received'},
+                            status_code=400)
+        if amount_received < price:
+            return Response(body={'error': 'Insufficient amount received'},
+                            status_code=400)
+        change = amount_received - price
+        return {'status': 'success', 'change': change}
+    
+    else:
+        return Response(body={'error': 'Invalid payment_method'},
+                        status_code=400)
